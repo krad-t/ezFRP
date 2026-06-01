90866ec8fdac3aed3560b88081dd5b502684d74b
krad <1249738064@qq.com>
2026-06-01 14:14:37 +0800

    feature(client&server):replace threading with selectors for TCP and UDP, single-thread I/O multiplexing

--- ezfrp_client.py
+++ ezfrp_client.py
@@ -10,18 +10,19 @@ class Client:
     def __init__(self):
         self.config = self.configure()
         self.sockets = []
-        self.sel = selectors.DefaultSelector()
+        self.tcp_sel = selectors.DefaultSelector()
+        self.udp_sel = selectors.DefaultSelector()
         self.server_control = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_control.connect((self.config['server_ip'], self.config['control_port']))
         self.sockets.append(self.server_control)
         self.control_thread = threading.Thread(target=self.handle_control, daemon=True)
         self.control_thread.start()
 
-
         self.sid2sock_map = dict()  # 维护一个socket映射表，key是来自服务端UDP包的session_id，value是本地的的socket对象实例
         self.sock2sid_map = dict() # 维护一个反向映射表，key是本地的socket对象实例，value是来自session_id
 
-    def configure(self):
+    @staticmethod
+    def configure():
         import json
         try:
             with open('ezfrp_client.json', 'r') as f:
@@ -39,6 +40,16 @@ class Client:
                 f.write(json.dumps(config))
         return config
 
+    @staticmethod
+    def pack_udp(sid: int, data: bytes, method="!I") -> bytes:
+        return struct.pack(method, sid) + data
+
+    @staticmethod
+    def unpack_udp(data: bytes, method="!I") -> (int, bytes):
+        session_id = struct.unpack(method, data[:4])[0]
+        packet_data = data[4:]
+        return session_id, packet_data
+
     def handle_control(self):
         print("Successfully connected to server", self.config['server_ip'])
         while True:
@@ -46,69 +57,66 @@ class Client:
             if choice == '1':
                 self.server_control.send(bytes('TCP',"utf-8"))
                 # daemon继承
-                threading.Thread(target=self.handle_control_tcp).start()
+                # threading.Thread(target=self.handle_control_tcp).start()
+                self.handle_control_tcp()
                 break
             elif choice == '2':
                 self.server_control.send(bytes('UDP',"utf-8"))
                 client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                client_sock.bind(("0.0.0.0", 0)) # 绑定一个随机端口
+                client_sock.bind(("0.0.0.0", 0)) # 绑定一个随机端口作为和Server转发UDP data的出入口
                 
                 # threading.Thread(target=self.handle_control_udp, args=(client_sock,)).start()
                 self.handle_control_udp(client_sock)
                 break
 
-
     def handle_control_udp(self, client_socket: socket.socket):
-        # udp
-        def local2client(socket_in_map: socket.socket):
-            while True:
-                data_local, _ = socket_in_map.recvfrom(4096)
-                data_local = struct.pack("!I", self.sock2sid_map[socket_in_map]) + data_local
-                client_socket.sendto(data_local, (self.config['server_ip'], self.config['udp_data_port'])) # 转发给server的UDP数据端口
-
-        def client2local():
-            while True:
-                client_data, _ = client_socket.recvfrom(4096)
-                sid = struct.unpack("!I", client_data[:4])[0]
-                print(f"client data: {client_data}")
-                client_data = client_data[4:]
-                if sid not in self.sid2sock_map: # 如果这个session_id之前没有连接过，那么就给他分配一个新的socket
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    sock.bind(("127.0.0.1", 0)) # 绑定一个随机端口
-                    self.sid2sock_map[sid] = sock
-                    self.sock2sid_map[sock] = sid
-                    threading.Thread(target=local2client, args=(sock,)).start() # 每个socket都要单独开一个线程来监听本地服务的响应数据，并转发给client
-                
-                sock = self.sid2sock_map[sid]
-                sock.sendto(client_data, (self.config['local_host'], self.config['local_port'])) # 转发给本地服务
-        
         print(f"client socket bound to {client_socket.getsockname()}")
-        self.server_control.send(bytes(str(client_socket.getsockname()[1]).encode("utf-8"))) # 把这个随机端口发给server，让server知道往哪个端口发UDP数据
-        # 但是如果Client处于NAT设备之后，那么这个端口不等于NAT设备映射出来的端口，所以还需要UDP打洞来获取正确的地址
-        cmd = self.server_control.recv(1024).decode("utf-8") # 等待接收Server打洞的指令，这时候Client就可以发送一个UDP包过去，让Server判断收到的UDP包是否和通过server_control发过来的端口一致，从而确认Client的网络拓扑
+        cmd = self.server_control.recv(1024).decode("utf-8")
+        # 等待接收Server打洞的指令，然后Client就可以发送一个UDP包过去，让Server知道被NAT映射后的端口
         print(f"client command received: {cmd}")
         if cmd == "UDP_HOLE_PUNCHING":
-            data = struct.pack("!I", 0)
+            data = Client.pack_udp(0,b'UDP_HOLE_PUNCHING')
             client_socket.sendto(data, (self.config['server_ip'], self.config['udp_data_port'])) # NAT 打洞
-
 
         def keepalive():
             import time
             while True:
                 time.sleep(10)
-                data = struct.pack("!I", 0)
+                data = Client.pack_udp(0,b'UDP_KEEPALIVE')
                 client_socket.sendto(data, (self.config['server_ip'], self.config['udp_data_port']))
 
         threading.Thread(target=keepalive, daemon=True).start()
 
-        threading.Thread(target=client2local).start()
+        self.udp_sel.register(client_socket, selectors.EVENT_READ, data=("->local",0))
+        while True:
+            events = self.udp_sel.select()
+            for key, mask in events:
+                if mask & selectors.EVENT_READ:
+                    origin_sock = cast(socket.socket, key.fileobj)
+                    packet_type = key.data[0]
+                    if packet_type == "->local":
+                        packet_with_sid, _ = origin_sock.recvfrom(4096)
+                        sid, packet_data = Client.unpack_udp(packet_with_sid)
+                        if sid not in self.sid2sock_map:
+                            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                            sock.bind((self.config['local_host'], 0))
+                            self.sid2sock_map[sid] = sock
+                            self.sock2sid_map[sock] = sid
+                            self.udp_sel.register(sock, selectors.EVENT_READ, data=("->server",sid))
+
+                        sock = self.sid2sock_map[sid]
+                        sock.sendto(packet_data,(self.config['local_host'], self.config['local_port']))
+                    elif packet_type == "->server":
+                        packet_data, _ = origin_sock.recvfrom(4096)
+                        sid = key.data[1]
+                        packet_with_sid = Client.pack_udp(sid, packet_data)
+                        client_socket.sendto(packet_with_sid, (self.config['server_ip'], self.config['udp_data_port']))
+
 
     def handle_control_tcp(self):
-        # tcp
-        # 第一次是发送控制消息new，此消息表示"有新的外部用户连接了，client可以创建data socket了" 只接受一个new指令
-        self.sel.register(self.server_control, selectors.EVENT_READ, data="NewUser")
+        self.tcp_sel.register(self.server_control, selectors.EVENT_READ, data="NewUser")
         while True:
-            events = self.sel.select()
+            events = self.tcp_sel.select()
             for key, mask in events:
                 data = key.data
                 if data == 'NewUser':
@@ -122,23 +130,23 @@ class Client:
                         local_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                         local_data.connect((self.config['local_host'], self.config['local_port']))
                         self.sockets.append(local_data)
-                        self.sel.register(server_data, selectors.EVENT_READ, data=local_data)
-                        self.sel.register(local_data, selectors.EVENT_READ, data=server_data)
+                        self.tcp_sel.register(server_data, selectors.EVENT_READ, data=local_data)
+                        self.tcp_sel.register(local_data, selectors.EVENT_READ, data=server_data)
                 elif isinstance(data, socket.socket):
                     sock_a = cast(socket.socket, key.fileobj)
                     sock_b = key.data
                     try:
                         recv_data = sock_a.recv(1024)
                         if not recv_data:
-                            self.sel.unregister(sock_a)
-                            self.sel.unregister(sock_b)
+                            self.tcp_sel.unregister(sock_a)
+                            self.tcp_sel.unregister(sock_b)
                             sock_a.close()
                             sock_b.close()
                         else:
                             sock_b.send(recv_data)
                     except (ConnectionResetError, OSError):
-                        self.sel.unregister(sock_a)
-                        self.sel.unregister(sock_b)
+                        self.tcp_sel.unregister(sock_a)
+                        self.tcp_sel.unregister(sock_b)
                         sock_a.close()
                         sock_b.close()
 
--- ezfrp_server.py
+++ ezfrp_server.py
@@ -8,7 +8,8 @@ from typing import cast
 class Server:
     def __init__(self):
         config = Server.configure()
-        self.sel = selectors.DefaultSelector()
+        self.tcp_sel = selectors.DefaultSelector()
+        self.udp_sel = selectors.DefaultSelector()
         self.sockets = []
         self.server_control = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_control.bind(("0.0.0.0", config['control_port']))
@@ -56,6 +57,16 @@ class Server:
                 f.write(json.dumps(config))
         return config
 
+    @staticmethod
+    def pack_udp(sid: int, data: bytes, method="!I") -> bytes:
+        return struct.pack(method, sid) + data
+
+    @staticmethod
+    def unpack_udp(data: bytes, method="!I") -> (int, bytes):
+        session_id = struct.unpack(method, data[:4])[0]
+        packet_data = data[4:]
+        return session_id, packet_data
+
     def handle_control(self):
         # 监听 :7000，处理 Client 的控制消息
         # control_channel 专门用来处理控制消息
@@ -70,7 +81,8 @@ class Server:
                     print(f"CMD [{client_cmd}] from {addr}")
                     # daemon继承
                     if client_cmd == 'TCP':
-                        threading.Thread(target=self.handle_public_tcp, args=(control_channel,)).start()
+                        # threading.Thread(target=self.handle_public_tcp, args=(control_channel,)).start()
+                        self.handle_public_tcp(control_channel)
                     elif client_cmd == 'UDP':
                         # threading.Thread(target=self.handle_public_udp, args=(control_channel,)).start()
                         self.handle_public_udp(control_channel)
@@ -79,55 +91,48 @@ class Server:
                     break
 
     def handle_public_udp(self, control_channel: socket.socket):
-        # 监听 :9998，处理外部用户连接
-        def user2client(client_addr):
-            while True:
-                data_user, addr_user = self.server_public_udp.recvfrom(4096)
-                print(f"Data [{data_user}] from {addr_user}")
-                if addr_user not in self.addr2sid_map:  # 如果这个用户之前没有连接过，那么就给他分配一个新的session_id
-                    self.session_sequence += 1
-                    session_id = self.session_sequence
-                    self.addr2sid_map[addr_user] = session_id  # 建立用户地址到session_id的映射
-                else:  # 如果这个用户之前连接过，那么就复用之前的session_id
-                    session_id = self.addr2sid_map[addr_user]
-
-                self.sid2addr_map[session_id] = addr_user  # 建立session_id到用户地址的映射
-
-                data_user = struct.pack("!I", session_id) + data_user
-                self.server_client_udp_data.sendto(data_user, client_addr)
-
-        def client2user():
-            while True:
-                data_client, _ = self.server_client_udp_data.recvfrom(4096)
-                session_id = struct.unpack("!I", data_client[:4])[0]
-                data_client = data_client[4:]
-                if session_id != 0:  # UDP 打洞的包，忽略
-                    if session_id in self.sid2addr_map:  # 如果这个session_id存在，那么就拿到对应的用户地址，转发给用户
-                        user_addr = self.sid2addr_map[session_id]
-                        self.server_public_udp.sendto(data_client, user_addr)
-
-        client_port_udp = control_channel.recv(1024).decode('utf-8')  # 等待client回复自己的udp地址
-        client_addr_udp = (control_channel.getpeername()[0], int(client_port_udp))  # 拼出client的udp地址
         # 若client 处于 NAT 设备之后，则这个端口就是错误的，需要靠UDP打洞来获取正确的地址
         control_channel.send(bytes("UDP_HOLE_PUNCHING", "utf-8"))  # 发送打洞指令给client，让client发送一个UDP包过来
-        _, addr = self.server_client_udp_data.recvfrom(4096)  # 从client发过来的UDP包中获取打洞后的端口
-        client_port_udp_nat = addr[1] if addr[1] != client_addr_udp[1] else client_addr_udp[1]
+        _, client_port_udp_nat = self.server_client_udp_data.recvfrom(4096)  # 从client发过来的UDP包中获取打洞后的端口
+        client_port_udp_nat = client_port_udp_nat[1]
         client_addr_udp = (control_channel.getpeername()[0], int(client_port_udp_nat))
         print(f"Client's UDP addr is                   {control_channel.getpeername()[0]}")
-        print(f"Client's UDP port in LAN is            {client_port_udp}")
         print(f"Client's UDP port by mapping in NAT is {client_port_udp_nat}")
-        print(f"The Client is after NAT: {client_port_udp != client_port_udp_nat}")
 
-        # 双向转发
-        threading.Thread(target=user2client, args=(client_addr_udp,)).start()
-        threading.Thread(target=client2user).start()
+        self.udp_sel.register(self.server_public_udp, selectors.EVENT_READ, data="->client")
+        self.udp_sel.register(self.server_client_udp_data, selectors.EVENT_READ, data="->user")
+
+        while True:
+            events = self.udp_sel.select()
+            for key, mask in events:
+                if mask & selectors.EVENT_READ:
+                    origin_sock = cast(socket.socket, key.fileobj)
+                    packet_type = key.data
+                    if packet_type == "->client":
+                        packet_data, addr = origin_sock.recvfrom(4096)
+                        if addr not in self.addr2sid_map:
+                            self.session_sequence += 1
+                            sid = self.session_sequence
+                            self.addr2sid_map[addr] = sid
+                            self.sid2addr_map[sid] = addr
+                        else:
+                            sid = self.addr2sid_map[addr]
+                        packet_data_with_sid = Server.pack_udp(sid, packet_data)
+                        self.server_client_udp_data.sendto(packet_data_with_sid, client_addr_udp)
+                    elif packet_type == "->user":
+                        packet_data_with_sid, _ = origin_sock.recvfrom(4096)
+                        session_id, packet_data = Server.unpack_udp(packet_data_with_sid)
+                        if session_id != 0:
+                            if session_id in self.sid2addr_map:
+                                user_addr = self.sid2addr_map[session_id]
+                                self.server_public_udp.sendto(packet_data, user_addr)
+                        elif session_id == 0:
+                            print(str(packet_data))
 
     def handle_public_tcp(self, control_channel: socket.socket):
-        # 监听 :9999，处理外部用户连接
-
-        self.sel.register(self.server_public_tcp, selectors.EVENT_READ, data="NewUser")
+        self.tcp_sel.register(self.server_public_tcp, selectors.EVENT_READ, data="NewUser")
         while True:
-            events = self.sel.select()
+            events = self.tcp_sel.select()
             for key, mask in events:
                 if mask & selectors.EVENT_READ:
                     data = key.data
@@ -137,8 +142,8 @@ class Server:
                         control_channel.send(b'new')
                         conn_client, addr2 = self.server_control.accept()
                         print(f"Connected by client: {addr2}")
-                        self.sel.register(conn_user, selectors.EVENT_READ, data=conn_client)
-                        self.sel.register(conn_client, selectors.EVENT_READ, data=conn_user)
+                        self.tcp_sel.register(conn_user, selectors.EVENT_READ, data=conn_client)
+                        self.tcp_sel.register(conn_client, selectors.EVENT_READ, data=conn_user)
                         self.sockets.append(conn_user)
                         self.sockets.append(conn_client)
                     elif isinstance(data, socket.socket):
@@ -147,16 +152,16 @@ class Server:
                         try:
                             recv_data = sock_a.recv(1024)
                             if not recv_data:
-                                self.sel.unregister(sock_a)
-                                self.sel.unregister(sock_b)
+                                self.tcp_sel.unregister(sock_a)
+                                self.tcp_sel.unregister(sock_b)
                                 sock_a.close()
                                 sock_b.close()
                             else:
                                 sock_b.sendall(recv_data)
                         except (ConnectionResetError, OSError):
                             # 关 socket 前必须清理注册 必须 unregister 先于 close——close 后 FD 失效，再 unregister 会报错。
-                            self.sel.unregister(sock_a)
-                            self.sel.unregister(sock_b)
+                            self.tcp_sel.unregister(sock_a)
+                            self.tcp_sel.unregister(sock_b)
                             sock_a.close()
                             sock_b.close()
 
